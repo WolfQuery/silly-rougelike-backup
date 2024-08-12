be82fe191d49ddae3dc5dcc2e7c5cb92a563bb81
WolfQuery <165044231+WolfQuery@users.noreply.github.com>
2024-08-12 21:03:11 +0200

    Disabled gravity for player

--- Gameplay/Player/player.gd
+++ Gameplay/Player/player.gd
@@ -5,8 +5,8 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+# disabled as game is top down and hopefully wont be needed (prone to change later as this is a quick fix)
 
 func _physics_process(delta):
 	# Add the gravity.

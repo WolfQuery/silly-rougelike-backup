e583c93d17a3341b564e2c4c9d81e111a5449e2f
WolfQuery <qwertman783@outlook.com>
2024-08-13 14:58:02 +0200

    this commit has a mistake in the code and will be reverted

--- Gameplay/Player/player.gd
+++ Gameplay/Player/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 var speed = Globals.SPEED 
 var health = Globals.HEALTH
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 

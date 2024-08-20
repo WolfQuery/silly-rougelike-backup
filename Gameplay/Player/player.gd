1cd776e08f34965d6d57360b5ad075d9df21f787
WolfQuery <qwertman783@outlook.com>
2024-08-20 13:20:37 +0200

    added a PROPER PLAYER AND SCRAPED GENERATION

--- Gameplay/Player/player.gd
+++ Gameplay/Player/player.gd
@@ -1,18 +1,23 @@
 extends CharacterBody2D
 
-
 var speed = Globals.SPEED 
 var health = Globals.HEALTH
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-# disabled as game is top down and hopefully wont be needed (prone to change later as this is a quick fix)
+@onready var animation_player = $AnimatedSprite2D
 
 func _physics_process(_delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
-
+	play_animations(direction)
 	move_and_slide()
+
+func play_animations(direction):
+	if direction == Vector2.ZERO:
+		animation_player.play("idle")
+	elif direction.x != 0 or direction.y != 0:
+		animation_player.play("run")
+	if Input.is_action_just_pressed("attack"):
+		animation_player.play("attack")
